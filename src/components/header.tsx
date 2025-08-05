f634f2c82e6732e314db163ff3269cce61d10b69
ankur shahi <ankurshahi372@gmail.com>
2025-08-05 16:23:56 +0000

    raksha bandhan ki badhyi ki heading hai ye sab ke liye hota hai but ye p

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,12 +7,12 @@ const Header = () => {
       <div className="inline-flex items-center justify-center">
         <RakhiIcon className="h-12 w-12 text-primary -mr-2" />
         <h1 className="font-headline text-5xl md:text-7xl font-bold text-primary-foreground text-shadow-custom select-none">
-          Raksha Bandhan ki Badhai
+          Raksha Bandhan ki Yaadein
         </h1>
         <RakhiIcon className="h-12 w-12 text-primary -ml-2" />
       </div>
       <p className="mt-4 text-lg text-muted-foreground font-body">
-        Bhai-behen ke anmol rishte ka ek digital album.
+        Remote ki ladai se lekar Maggi ki aakhri bite tak ka safar.
       </p>
     </header>
   );
